1469ffeb5547afa0cab5e0f3b237fb0e03559da7
Tatuli Dushuashvili <tatulidushuashvili@192.168.0.118>
2025-05-29 03:11:30 +0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+// import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductsComponent,
+    AboutComponent,
+    ContactComponent
+    
   ],
   imports: [
     BrowserModule,
